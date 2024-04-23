0983fa40b92109223a9e1fb880a36fe26b2fb7e1
Amobu0 <kdehgogo104@gmail.com>
2024-04-23 16:53:04 +0900

    4/23 sangdo

--- src/main/java/kr/co/lotte/repository/MemberRepository.java
+++ src/main/java/kr/co/lotte/repository/MemberRepository.java
@@ -4,7 +4,11 @@ import kr.co.lotte.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<User, String> {
 
+    Optional<User> findByEmail(String email);
 }

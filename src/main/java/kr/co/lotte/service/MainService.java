2192b84fd3015d88efc69eaa7198c4cfcf1d4db5
gahuileeee <gahui446@gmail.com>
2024-05-02 17:10:13 +0900

    commit

--- src/main/java/kr/co/lotte/service/MainService.java
+++ src/main/java/kr/co/lotte/service/MainService.java
@@ -21,7 +21,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -32,7 +31,6 @@ import java.util.List;
 @Slf4j
 @Transactional
 @RequiredArgsConstructor
-@Transactional
 public class MainService {
     @Autowired
     private ProductsRepository productsRepository;
